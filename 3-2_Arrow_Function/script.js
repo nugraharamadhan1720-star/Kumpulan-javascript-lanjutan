4bc7e552e7401860feaa5e194bfeeb985d059f0a
nugraha wahid ramadhan <nugraha.ramadhan1720@gmail.com>
2025-09-10 13:35:38 +0700

    menambahkan folder 3-2 arrow function

---
+++ 3-2_Arrow_Function/script.js
@@ -0,0 +1,76 @@
+// konsep this pada Arrow Funcition
+
+//  contructor funcition
+// const Mahasiswa = function() {
+//     this.nama = ' nugraha';
+//     this.umur = 22;
+//     this.sayHelo = function () {
+//         console.log('Halo, nama saya ${this.nama}, dan saya ${umur} tahun.');
+//     }
+// }
+
+// const nugraha = new Mahasiswa ();
+
+
+// Arrow Function
+// const Mahasiswa = function () {
+//     this.nama = ' nugraha';
+//     this.umur = 22;
+//     this.sayHelo = () => {
+//         console.log('Halo, nama saya ${this.nama}, dan saya ${umur} tahun.');
+//     }
+// }
+
+// const nugraha = new Mahasiswa ();
+
+
+
+
+
+// object literal
+// const mhs1 = {
+//     nama : 'nugraha',
+//     umur : 22,
+//     sayHello: function () {
+//         console.log('Halo, nama saya ${this.nama}, dan saya ${umur} tahun.');
+//     }
+// }
+
+
+
+
+// const Mahasiswa = function() {
+//     this.nama = ' nugraha';
+//     this.umur = 22;
+//     this.sayHelo = function () {
+//         console.log('Halo, nama saya ${this.nama}, dan saya ${umur} tahun.');
+//     }
+
+//     setInterval(()=> {
+//         console.log(this.umur++);
+//     }, 500);
+// }
+
+// const nugraha = new Mahasiswa ();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
