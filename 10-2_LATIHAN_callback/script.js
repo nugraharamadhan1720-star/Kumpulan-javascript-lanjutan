833286a78077ef500dc81e661fe548cbf7dc1d43
Agung Trisutaji Aprian <agung.trisutaji.aprian@gmail.com>
2025-09-10 16:59:58 +0700

    fix yang tertera pada setiap // TODO:

--- 10-2_LATIHAN_callback/script.js
+++ 10-2_LATIHAN_callback/script.js
@@ -1,9 +1,13 @@
-$.ajax( {
-  url: 'http://www.omdbapi.com/?apikey=294c8cf6&=avengers',
-  succes: results => {
+$.ajax({
+  // TODO: Incorrect Parameter "?apikey=294c8cf6&=avengers" should be "?apikey=294c8cf6&t=avengers". Missing "s=" for title
+  url: 'https://www.omdbapi.com/?apikey=294c8cf6&=avengers',
+
+  // TODO: Misspelled word "succes" should be "success"
+  succes: (results) => {
     const movies = results.Search;
     let cards = '';
     movies.forEach(m => {
+      // TODO: Misspelled word "<a href="#" class="btn btn-primary">Show Detaills</a>" should be "Details"
       cards += `<div class="col-md-4 my-3">
                 <div class="card">
                     <img src="${m.Poster}" class="card-img-top">
@@ -17,6 +21,7 @@ $.ajax( {
     });
     $('.movie-container').html(cards);
   },
+  // TODO: typo "Error" should be "error"
   Error: (e) => {
     console.log(e.responseText);
   }
