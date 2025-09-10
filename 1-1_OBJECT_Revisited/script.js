fc7f6be8dceb9b6e27c02ef835ae68b547c4b70d
nugraha wahid ramadhan <nugraha.ramadhan1720@gmail.com>
2025-09-10 13:29:56 +0700

    menambahkan folder 1-1 object revisited

---
+++ 1-1_OBJECT_Revisited/script.js
@@ -0,0 +1,70 @@
+// cara untuk membuatobject pada javascript
+// 1. object Literal
+// let mahasiswa1 = {
+//     nama: ' Nugraha',
+//     energy: 10,
+//     makan: function(porsi) {
+//         this.energin = this.energi + porsi ;
+//         console.log('Halo ${this.nama} selamat makan!');
+//     }
+// } 
+
+
+// let mahasiswa2 = {
+//     nama: 'Dody',
+//     energy: 20,
+//     makan: function(porsi) {
+//         this.energin = this.energi + porsi ;
+//         console.log('Halo ${this.nama} selamat makan!');
+//     }
+// }
+
+
+
+
+// 2. function declaration
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa ={};
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
+
+//     mahasiswa.makan = function (porsi) {
+//         this.energi  += porsi;
+//         console.log('Halo ${this.nama}, selamat makan!');
+//     }
+
+//     mahasiswa.main = function (jam) {
+//         this.energi -= jam;
+//         console.log('Halo ${this.nama}, selamat bermain!');
+//     }
+
+//     return mahasiswa;
+// }
+
+// let Nugraha = Mahasiswa('Nugraha', 10);
+// let doddy = Mahasiswa('Doddy', 20);
+
+
+
+
+// 3. constructor function
+
+function Mahasiswa(nama, energi) {
+    this.nama = nama;
+    this.energi = energi;
+
+    this.makan = function (porsi) {
+        this.energi  += porsi;
+        console.log('Halo ${this.nama}, selamat makan!');
+    }
+
+    this.main = function (jam) {
+        this.energi -= jam;
+        console.log('Halo ${this.nama}, selamat bermain!');
+    }
+}
+
+let nugraha = new Mahasiswa('nugraha',10);
+
+
+// 4. object.create
