8f375c528100cf96ebaad92ce955b8803c5dfc7d
nugraha wahid ramadhan <nugraha.ramadhan1720@gmail.com>
2025-09-10 13:44:01 +0700

    menambahkan folder 6-1 destructuring assignment

---
+++ 6-1_Destructuring_Assignment/script.js
@@ -0,0 +1,50 @@
+// Destructuring Variable / Assigment
+
+// Destructuring Array
+// const perkenalan = ['Halo', 'nama', 'saya', 'Nugraha'];
+
+// // const [salam, satu, dua, nama] = perkenalan;
+
+// // skipping items
+// const [salam, , , nama] = perkenalan;
+// console.log(salam);
+
+// swap items
+// let a = 1;
+// let b = 2;
+// console.log(a);
+// console.log(b);
+// [a,b] = [b,a];
+// console.log(a);
+// console.log(b);
+
+// return value pada function
+// function coba(){
+//     return [1,2];
+// }
+
+// const [a, b] = coba();
+// console.log(a[1]);
+
+// Rest parameter
+// const [a, b] = [1, 2];
+// console.log(a);
+// console.log(b);
+
+
+// Destructuring object
+// const mhs = {
+//     nama: 'Nugraha',
+//     umur: 22
+// }
+
+// const {nama, umur } = mhs;
+// console.log(nama);
+
+
+// assigment tanpa deklarasi object
+
+// ({nama, umur } ={ nama: 'Nugraha',umur: 22});
+// console.log(nama);
+
+ 
