cc7fb89de124dcc84e32552b3ea096a28855e504
nugraha wahid ramadhan <nugraha.ramadhan1720@gmail.com>
2025-09-10 13:41:10 +0700

    menambahkan folder 5-1 template literals

---
+++ 5-1_Template_Literals/script.js
@@ -0,0 +1,27 @@
+// Template Literals
+// const nama = 'Nugraha';
+// const umur = 22;
+// console.log('Halo, nama saya ${nama}, dan saya ${umur} tahun,');
+// console.log ('Halo, nama saya ' + nama + ', dan saya ' + umur + 'tahun.');
+
+// Embedded Exp Void
+// console.log('${ 1 + 1}');
+// console.log('${alert('halo!')}');
+// const x = 11;
+// console.log(`${(x % 2 == 0) ? 'genap' : 'ganjil'}`);
+
+
+// HTML Fragments
+// const mhs = {
+//     nama: 'Nugraha',
+//     umur: 22,
+//     nrp:'19220226',
+//     email:"nugraha.ramadhan1720@gmail.com"
+// };
+
+// const el = `<div class="mhs">
+//     <h2>${mhs.nama}</h2>
+//     <span class="nrp">${mhs.nrp}</span>
+// </div>`;
+
+// console.log(el);
