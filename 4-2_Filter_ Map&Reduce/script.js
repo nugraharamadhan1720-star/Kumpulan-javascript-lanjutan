423ff48fb25db722ec6ece875c7db55b9b7f6f1a
nugraha wahid ramadhan <nugraha.ramadhan1720@gmail.com>
2025-09-10 13:36:45 +0700

    menambahkan folder 4-2 filter map & reduce

---
+++ 4-2_Filter_ Map&Reduce/script.js
@@ -0,0 +1,35 @@
+const angka = [-1, 8, 9, 1, 4, -5, -4, 3, 2, 9];
+
+// mencari angka >=3
+// for
+// const newAngka = [];
+// for (let i = 0; i < angka.length; i++) {
+//     if( angka[i] >=3) {
+//         newAngka.push(angka[i]);
+//     }
+// }
+// console.log(newAngka);
+
+// filter
+// const newAngka = angka.filter(a => a >= 3);
+// console.log(newAngka);
+
+// Map
+// kalikan semua angka dengan 2
+// const newAngka = angka.map(a => a * 2);
+// console.log(newAngka);
+
+// reduce
+// jumlahkan seluruh elemen pada array
+// const newAngka = angka.reduce((accumulator, currentValue) =>accumulator + currentValue,5);
+// console.log(newAngka);
+
+
+// Method chaining
+// cari angka > 5
+// kalikan 3
+// jumlahkan
+const hasil = angka.filter(a => a > 5)
+.map(a => a * 3)
+.reduce((acc, cur) => acc + Cur);
+console.log(hasil);
