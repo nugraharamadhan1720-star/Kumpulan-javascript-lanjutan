97fbab2b39c7c14e4522e7d3a00d24ac66ca6687
nugraha wahid ramadhan <nugraha.ramadhan1720@gmail.com>
2025-09-11 11:44:12 +0700

    issue detail undefined

---
+++ 12_Fetch/script.js
@@ -0,0 +1,130 @@
+// $('.search-button').on('click', function() {
+
+//   $.ajax({
+//     // TODO: Incorrect Parameter "?apikey=294c8cf6&=avengers" should be "?apikey=294c8cf6&t=avengers". Missing "s=" for title
+//     url: 'https://www.omdbapi.com/?apikey=294c8cf6&s=' + $('.input-keyword').val(),
+  
+//     // TODO: Misspelled word "succes" should be "success"
+//     success: results => {
+//       const movies = results.Search;
+//       let cards = '';
+//       movies.forEach(m => {
+//         // TODO: Misspelled word "<a href="#" class="btn btn-primary">Show Detaills</a>" should be "Details"
+//         cards += showCards(m);       
+//       });
+//       $('.movie-container').html(cards);
+  
+  
+  
+//       // ketika tombol di klik
+  
+//       $('.modal-detail-button').on('click', function () {
+//         $.ajax({
+//           url: 'https://www.omdbapi.com/?apikey=294c8cf6&i=' + $(this).data('imdbid'),
+//           success: m => {
+//             const movieDetail = showMovieDetail(m);
+//            $('.modal-body').html(movieDetail);
+//           },
+//           error: (e) => {
+//       console.log(e.responseText);
+//     }
+//         });
+//       });
+  
+//     },
+//     // TODO: typo "Error" should be "error"
+//     error: (e) => {
+//       console.log(e.responseText);
+//     }
+    
+//   });
+  
+// }); 
+
+
+
+
+// fetch
+
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', function() {
+
+  const inputKeyword = document.querySelector('.input-keyword');
+fetch('https://www.omdbapi.com/?apikey=294c8cf6&s=' + inputKeyword.value)
+ .then(response => response.json())
+ .then(response => {
+  const movies = response.Search;
+  let cards ='';
+  movies.forEach(m => cards += showCards(m));
+  const movieContainer = document.querySelector('.movie-container');
+  movieContainer.innerHTML = cards;
+
+
+  // ketika tombol detail di-klik
+const modalDetailButton = document.querySelectorAll('.modal-detail-button');
+modalDetailButton.forEach(btn => {
+ btn.addEventListener('click', function() {
+  const imdbid = this.dataset.imdbid;
+  fetch('https://www.omdbapi.com/?apikey=294c8cf6&s=' + imdbid)
+    .then(response => response.json())
+    .then(m => {
+      const movieDetail = showMovieDetail(m);
+      const modalBody = document.querySelector('.modal-body');
+      modalBody.innerHTML = movieDetail;
+    });
+    });
+ });
+});
+
+
+
+ });
+
+// });
+
+
+
+
+
+
+
+
+
+
+function showCards(m) {
+  return `<div class="col-md-4 my-3">
+                <div class="card">
+                    <img src="${m.Poster}" class="card-img-top">
+                     <div class="card-body">
+                        <h5 class="card-title">${m.Title}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">${m.Year}</h6>
+                         <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+                    </div>
+                </div>
+            </div>`;  
+}
+
+
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+                              <div class="row">
+                                <div class="col-md-3">
+                                  <img src="${m.Poster}" class="img-fluid">
+                                </div>
+                                <div class="col-md">
+                                  <ul class="list-group">
+                                    <ul class="list-group">
+                                    <li class="list-group-item"><h4> ${m.Title} (${m.Year})</h4></li>
+                                    <li class="list-group-item"><strong>Director : </strong> ${m.Director}</li>
+                                    <li class="list-group-item"><strong>Actors : </strong> ${m.Actors}</li>
+                                    <li class="list-group-item"><strong>Writer : </strong> ${m.Writer}</li>
+                                    <li class="list-group-item"><strong>Plot : </strong> <br> ${m.Plot}</li>
+                                  </ul>
+                                </div>
+                              </div>
+                            </div>`;
+}
+
+
+
+
